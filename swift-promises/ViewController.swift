bb37a0bba2ac327c4e3b90ebfcd4fa008741844b
Oleksandr <oleksandr@elements.nl>
2019-04-03 20:27:30 +0200

    Create project

---
+++ swift-promises/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-promises
+//
+//  Created by Oleksandr  on 03/04/2019.
+//  Copyright © 2019 Elements Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
