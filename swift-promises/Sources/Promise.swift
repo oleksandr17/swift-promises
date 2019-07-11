f7dd9a179f28d71dcedb3393cf7db74d6f786b96
Oleksandr <oleksandr@elements.nl>
2019-07-11 08:41:24 +0200

    Re-write chaining, add log and transform operations

--- swift-promises/Sources/Promise.swift
+++ swift-promises/Sources/Promise.swift
@@ -31,3 +31,57 @@ class Promise<ValueType>: Future<ValueType> {
         self.result = .failure(error)
     }
 }
+
+extension Future {
+    func chain<NextValueType>(closure: @escaping (_ value: ValueType) -> Future<NextValueType>) -> Future<NextValueType> {
+        let promise = Promise<NextValueType>()
+        
+        observe { (result) in
+            switch result {
+            case let .success(value):
+                let nestedPromise = closure(value)
+                nestedPromise.observe(callback: { (nestedResult) in
+                    switch nestedResult {
+                    case let .success(nestedValue):
+                        promise.succeed(value: nestedValue)
+                        
+                    case let .failure(error):
+                        promise.fail(error: error)
+                    }
+                })
+                
+            case let .failure(error):
+                promise.fail(error: error)
+            }
+        }
+        return promise
+    }
+}
+
+extension Future {
+    func log() -> Future<ValueType> {
+        return chain { (value) -> Future<ValueType> in
+            let promise = Promise<ValueType>()
+            DispatchQueue.main.async {
+                print("Value: \(value)")
+                promise.succeed(value: value)
+            }
+            return promise
+        }
+    }
+    
+    func transform<NextValueType>(modifier: @escaping (ValueType) throws -> NextValueType) -> Future<NextValueType> {
+        return chain(closure: { (value) -> Future<NextValueType> in
+            let promise = Promise<NextValueType>()
+            DispatchQueue.global(qos: .background).async {
+                do {
+                    let newValue = try modifier(value)
+                    promise.succeed(value: newValue)
+                } catch {
+                    promise.fail(error: error)
+                }
+            }
+            return promise
+        })
+    }
+}
